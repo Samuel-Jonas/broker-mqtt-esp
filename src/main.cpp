cfc0fa86dbffd861696d2bb348135fe45a5677ee
Samuel-Jonas <samueljonas@alu.ufc.br>
2024-02-22 09:40:57 -0300

    feat: broker running inside esp32  IP 198.162.0.5

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,43 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include "sMQTTBroker.h"
 
-// put function declarations here:
-int myFunction(int, int);
+const char* WIFI_SSID = "brisa-3091372";
+const char* WIFI_PASSWORD = "teskdl6u";
+const unsigned short MQTT_PORT = 1883;
+
+sMQTTBroker broker;
+
+//IPAddress local_IP(192, 168, 0, 3); // 192.168.0.3
+//IPAddress gateway(192, 168, 0, 1); // router IP gateway
+//IPAddress subnet(255, 255, 0, 0); // subnet mask
+
+void connect_to_wifi() {
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+  }
+
+  Serial.println("Connected to wifi");
+  Serial.print("IP address:\t");
+  Serial.println(WiFi.localIP());
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+
+  connect_to_wifi();
+
+  broker.init(MQTT_PORT);
+
+  //if (!WiFi.config(local_IP, gateway, subnet)) {
+    //Serial.println("Failed to configure wifi connection");
+  //}
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  delay(1000);
+  broker.update();
+  Serial.println("broker updated");
 }
